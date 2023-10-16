a89794d68df2963ecd239b124c6854f2158a9bf4
Ferfahl <fe.alves.fahl@gmail.com>
2023-10-16 08:44:59 -0300

    [fix] various fixes

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-mysqld_safe &
-service mysql start
+service mariadb start
 
 # Kill the anonymous users
-mysql -e "DROP USER IF EXISTS ''@'localhost'"
+mariadb -e "DROP USER IF EXISTS ''@'localhost'"
 
 # Kill off the demo database
-mysql -e "DROP DATABASE IF EXISTS test"
+mariadb -e "DROP DATABASE IF EXISTS test"
 
 # Create Wordpress database
-mysql -e "CREATE DATABASE $MYSQL_DATABASE"
+mariadb -e "CREATE DATABASE $MYSQL_DATABASE"
 
 # Create Mariadb/Wordpress user
-mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mariadb -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
 
 # Grant privileges to user
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%'"
+mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* to '$MYSQL_USER'@'%'"
 
 # Make sure that NOBODY can access the server without a password
-mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES"
+mariadb -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';FLUSH PRIVILEGES"
--- srcs/requirements/wordpress/tools/wp_install.sh
+++ srcs/requirements/wordpress/tools/wp_install.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-chown -R www-data:www-data /var/www/
+if ! wp core is-installed --allow-root --path=/var/www/wordpress; then
+    chown -R www-data:www-data /var/www/
+    sudo -u www-data sh -c "wp core download"
+    sudo -u www-data sh -c "wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset='utf8'"
+    sudo -u www-data sh -c "wp core install --url=$WORDPRESS_URL --title=Inception --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email"
+    sudo -u www-data sh -c "wp user create $WORDPRESS_GUEST_USER $WORDPRESS_GUEST_EMAIL --role=author --user_pass=$WORDPRESS_GUEST_PASSWORD"
+    sudo -u www-data sh -c "wp theme install twentysixteen --activate"
+    sudo -u www-data sh -c "wp plugin update --all"
 
-sudo -u www-data sh -c "wp core download"
-sudo -u www-data sh -c "wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=mariadb --dbcharset='utf8'"
-sudo -u www-data sh -c "wp core install --url=$WORDPRESS_URL --title=Inception --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email"
-sudo -u www-data sh -c "wp user create $WORDPRESS_GUEST_USER $WORDPRESS_GUEST_EMAIL --role=author --user_pass=$WORDPRESS_GUEST_PASSWORD"
-sudo -u www-data sh -c "wp plugin update --all"
-
-ln -s $(find /usr/sbin -name 'php-fpm*') /usr/bin/php-fpm
+    ln -s $(find /usr/sbin -name 'php-fpm*') /usr/bin/php-fpm
+else
+	echo "wordpress already downloaded"
+fi
 
 exec /usr/bin/php-fpm -F
